c994c1e0373f604e5c88bfaddfa321b6967f6e30
Anil Baidnath Pandit <87321054+anilpandit195@users.noreply.github.com>
2022-10-08 21:07:12 +0530

    Add files via upload

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid';
 
 const Home = () => {
+    const [Roomid, setRoomid] = useState('');
+    const [Username, setUsername] = useState('');
+    const creatnewroom = (e) => {
+        e.preventDefault();
+        const id = uuidv4();
+        setRoomid(id);
+
+    };
     return (
         <div className='homepageback'>
             <div className='homepageform'>
                 <img className="homepagelogo" src="/code-sync.png" alt="cpde-sync-logo" />
                 <h4 className="heading">Paste invitation ROOM ID</h4>
                 <div className="inputgroup">
-                    <input type="text" className="input" placeholder="Enter Room ID" />
-                    <input type="text" className="input" placeholder="Enter Your Name" />
+                    <input
+                        type="text"
+                        className="input"
+                        placeholder="Enter Room ID"
+                        onChange={(e) => setRoomid(e.target.value)}
+                        value={Roomid}
+                    />
+                    <input
+                        type="text"
+                        className="input"
+                        placeholder="Enter Your Name"
+                        onChange={(e) => setUsername(e.target.value)}
+                        value={Username}
+                    />
                     <button className="btn joinbtn">Join Room</button>
                     <span className="creatinfo">
                         If you don't have a room ID, Create a &nbsp;
-                        <a href="" className="newroom" >new room</a>
+                        <a onClick={creatnewroom} href="" className="newroom" >new room</a>
                     </span>
                 </div>
             </div>
             <footer className="footer">
-                <h4>Build by 😊 &nbsp; {''}
-                    <a href="anilpandit" >Anilpandit195</a>
+                <h4>Build by 😊 {''}
+                    <a href="https://github.com/anilpandit195" className="linkgit" >Anilpandit195</a>
                 </h4>
 
             </footer>
